3fba1996771b71206f67cf7c6cfd9137465ddaf8
gulong <gulong.chen@hotmail.com>
2024-11-01 11:16:06 +0800

    update config files

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const nextConfig = {
       { protocol: "https", hostname: "images.unsplash.com" },
       { protocol: "https", hostname: "img.clerk.com" },
       { protocol: "https", hostname: "gitee.com" },
+      { protocol: "https", hostname: "storage.guliucang.com"}
     ],
   },
   output: "standalone",
